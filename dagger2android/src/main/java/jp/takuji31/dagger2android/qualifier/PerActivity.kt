45e0bc4d78435be463695224cfdde7e26396c67b
Takuji Nishibayashi <takuji31@gmail.com>
2015-09-17 15:53:19 +0900

    PerActivity is scope

--- dagger2android/src/main/java/jp/takuji31/dagger2android/qualifier/PerActivity.kt
+++
@@ -1,8 +0,0 @@
-package jp.takuji31.dagger2android.qualifier
-
-import javax.inject.Qualifier
-
-/**
- * @author Takuji Nishibayashi
- */
-@Qualifier public annotation class PerActivity
---
+++ dagger2android/src/main/java/jp/takuji31/dagger2android/scope/PerActivity.kt
@@ -0,0 +1,8 @@
+package jp.takuji31.dagger2android.scope
+
+import javax.inject.Scope
+
+/**
+ * @author Takuji Nishibayashi
+ */
+@Scope public annotation class PerActivity
